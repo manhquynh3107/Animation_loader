6cc8f5d1237e4076033711f3d528c3b57c76b9d0
manhquynh3107 <manhquynh3107@gmail.com>
2020-12-20 12:56:15 +0700

    Animation

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Css Loader</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+<div class="planet planetEarth">
+    <h2>Earth</h2>
+    <div class="container"><span></span></div>
+    <div class="earth"></div>
+</div>
+</body>
+</html>
